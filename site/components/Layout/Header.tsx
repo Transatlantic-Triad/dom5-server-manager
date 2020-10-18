1c05042090eceee62c28ba6458ac283de5460050
Linn Dahlgren <admin@opposite.biz>
2020-10-18 22:11:50 +0200

    Added link to server config

--- site/components/Layout/Header.tsx
+++ site/components/Layout/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import CogIcon from 'mdi-react/CogIcon';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -13,12 +14,18 @@ export default function Header(): JSX.Element {
         </Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="w-100">
             <Link passHref href="/list">
               <Nav.Link>List games</Nav.Link>
             </Link>
             <Link passHref href="/new_game">
               <Nav.Link>New game</Nav.Link>
+            </Link>
+            <Link passHref href="/setup">
+              <Nav.Link className="d-inline-flex ml-sm-auto align-items-center">
+                <CogIcon />
+                <span className="ml-1 d-sm-none">Settings</span>
+              </Nav.Link>
             </Link>
           </Nav>
         </Navbar.Collapse>
